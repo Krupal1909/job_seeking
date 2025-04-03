04d6571c51238af8bf947cf401286c4c8f83c873
krupalpatelmks <krupal.mksdigitech@outlook.com>
2025-04-03 17:49:54 +0530

    add .gitignore file

--- Backend/Models/UserModel.js
+++ Backend/Models/UserModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jsonwebtoken = require("jsonwebtoken");
 const userModel = new mongoose.Schema({
   name: {
